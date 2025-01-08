2f2bf6a412bcda0bb63a3831cbaf4046d2f10ec5
pablomartinfranco <pafranco@frba.utn.edu.ar>
2025-01-08 00:51:33 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -332,7 +332,13 @@ require('lazy').setup({
   -- you do for a plugin at the top level, you can do for a dependency.
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
-
+  
+  -- randy: added this in
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+  },
+    
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
